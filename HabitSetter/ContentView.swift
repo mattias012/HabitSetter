94d84260b20a9e49f750c3790a1a589eae4e8a9d
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2024-04-21 23:06:03 +0200

    Initial Commit

---
+++ HabitSetter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitSetter
+//
+//  Created by Mattias Axelsson on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitSetter/HabitSetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitSetterApp.swift
+//  HabitSetter
+//
+//  Created by Mattias Axelsson on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct HabitSetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
